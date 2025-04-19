d6131ea4b806b12037fd5773ff716f58e7a217fe
practice-repo2 <maadi9396@gmail.com>
2025-04-19 17:14:40 +0530

    Database setup

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import admin from 'firebase-admin';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load your service account key
+const serviceAccountPath = path.join(__dirname, './credentials/speakeasykey.json');
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
+
+// Initialize Firebase Admin
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  
+});
+
+export default admin;
