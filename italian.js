f02a61cd8eb9036b6d3fb459cf5a18321a84e151
DEEPAK SHARMA <shivaay87081@gmail.com>
2025-04-25 12:29:23 +0530

    Added italian

---
+++ italian.js
@@ -0,0 +1,203 @@
+
+let questions = [
+{ 
+        question: 'Which one of these is "coffee"?', 
+        options: [
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/6183b0dcb2e5c90f9e440ad9650d41f8', text: 'cornetto', answer: false },
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703', text: 'voi', answer: false },
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/6fd84b8a838c43c4a84b44b08b10177e', text: 'caffè', answer: true }
+    ]
+},
+{
+        question: 'Which one of these is "red colour"?', 
+        options: [
+            { img: 'https://tse3.mm.bing.net/th?id=OIP.aIs1fPIdUcGu4luTwl9AiwHaHP&pid=Api&P=0&h=180', text: 'rosso', answer: true },
+            { img: 'https://tse2.mm.bing.net/th?id=OIP.z0PF6VnSA6IYiC96tFuYwQAAAA&pid=Api&P=0&h=180', text: 'blu', answer: false },
+            { img: 'https://tse4.mm.bing.net/th?id=OIP.Vx5WQTz2nmNue8gO4njZxQHaHa&pid=Api&P=0&h=180', text: 'nera', answer: false }
+        ]
+    },
+    { 
+        question: 'Which one of these is "The book"?', 
+        options: [
+            { img: 'pen-removebg-preview.png', text: 'penna', answer: false },
+            { img: 'https://tse2.mm.bing.net/th?id=OIP.Asg_j2qBjTgoj5q9ZXSNvgHaJb&pid=Api&P=0&h=180', text: 'libro', answer: true },
+            { img: 'pencil-removebg-preview.png', text: 'matita', answer: false }
+        ]
+    },
+    { 
+        question: 'Which one of these is "goodluck"?', 
+        options: [
+            { img: 'http://ts4.mm.bing.net/th?id=OIP.CNJHv58UPMM6GPrhzOLxDQHaHa&pid=15.1', text: 'ciao', answer: false },
+            { img: 'https://tse3.mm.bing.net/th?id=OIP.JHzUF4hRKa6A7hJnWuC4OQHaHa&pid=Api&P=0&h=180', text: 'buona fortuna', answer: true },
+            { img: 'https://th.bing.com/th/id/OIP.qVtLVukWzFHmmxGrd7Es0wHaHa?pid=ImgDet&w=184&h=184&c=7&dpr=1.3', text: 'laureato', answer: false }
+        ]
+    },
+    { 
+        question: 'How do you say "Hello"?', 
+        options: [
+            { img: 'http://ts4.mm.bing.net/th?id=OIP.CNJHv58UPMM6GPrhzOLxDQHaHa&pid=15.1', text: 'ciao', answer: true },
+            { img: 'https://th.bing.com/th/id/OIP.qVtLVukWzFHmmxGrd7Es0wHaHa?pid=ImgDet&w=184&h=184&c=7&dpr=1.3', text: 'laureato', answer: false },
+            { img: 'https://tse3.mm.bing.net/th?id=OIP.JHzUF4hRKa6A7hJnWuC4OQHaHa&pid=Api&P=0&h=180', text: 'buona fortuna', answer: false }
+        ]
+    },
+    {   question: 'Which one of these is "pen"?',
+        options:  [
+            { img: 'pen-removebg-preview.png',text:"penna" ,answer: true },
+            { img: 'pencil-removebg-preview.png',text: 'matita', answer: false },
+            { img: 'https://tse2.mm.bing.net/th?id=OIP.Asg_j2qBjTgoj5q9ZXSNvgHaJb&pid=Api&P=0&h=180',text:'libro', answer: false }
+        ]
+    },
+    {   question: 'which of these is "girl"?',
+        options:  [
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/a443582c1eb03f3fcebfff4febdba3e4',text:'ragazza', answer: true },
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/52275a140b3fb918b49449a08283ec80',text: 'donne', answer: false },
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/d6508c5ea6ebcb4859df33c80acd8d8c',text: 'il ragazzo' ,answer: false }
+        ]
+},
+    {   question: 'which of these is "pencil"?',
+        options:  [
+            { img: 'pencil-removebg-preview.png',text: 'matita', answer: true },
+            { img: 'pen-removebg-preview.png',text: 'penna', answer: false },
+            { img: 'https://tse2.mm.bing.net/th?id=OIP.Asg_j2qBjTgoj5q9ZXSNvgHaJb&pid=Api&P=0&h=180',text: 'libro', answer: false }
+        ]
+    },
+    {
+        question: 'Which one of these is "One"?', 
+        options: [
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/4be10d03a4c6ac4078b6be0df2eb5247', text: 'un', answer: true },
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/9aa862ebf2f8636383e54ce23d340db9', text: 'il gatto', answer: false },
+            { img: 'https://d2pur3iezf4d1j.cloudfront.net/images/b40a713e2c973cb54c3c5bb67ef19af4', text: 'l \'uomo', answer: false }
+        ]
+    },
+  ];
+
+  let currentQuestionIndex = 0;
+  let score = 0;
+  let timerInterval;
+
+  function speakText(text) {
+    if ('speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.rate = 1;
+      utterance.pitch = 1;
+      window.speechSynthesis.cancel();
+      window.speechSynthesis.speak(utterance);
+    }
+  }
+
+  function showPronounceModal(correctWord) {
+    const modal = document.getElementById('pronounceModal');
+    modal.style.display = 'flex';
+    document.getElementById('pronounceYes').onclick = () => {
+      modal.style.display = 'none';
+      speakText(correctWord);
+      showRepeatModal(correctWord);
+    };
+    document.getElementById('pronounceNo').onclick = () => {
+      modal.style.display = 'none';
+      currentQuestionIndex++;
+      loadQuestion();
+    };
+  }
+  function showRepeatModal(correctWord) {
+    const modal = document.getElementById('repeatModal');
+    modal.style.display = 'flex';
+    document.getElementById('repeatPronounce').onclick = () => {
+      speakText(correctWord);
+    };
+    document.getElementById('nextQuestion').onclick = () => {
+      modal.style.display = 'none';
+      currentQuestionIndex++;
+      loadQuestion();
+    };
+  }
+
+  function loadQuestion() {
+    document.getElementById('timerDisplay').style.display = 'inline-block';
+    if (currentQuestionIndex < questions.length) {
+      const questionData = questions[currentQuestionIndex];
+      document.getElementById('question').innerText = questionData.question;
+      const optionsDiv = document.getElementById('options');
+      optionsDiv.innerHTML = '';
+      questionData.options.forEach((option, idx) => {
+        const optionCard = document.createElement('div');
+        optionCard.classList.add('col-sm-4', 'col-md-3', 'card', 'm-2', 'p-2');
+        optionCard.setAttribute('data-answer', option.answer);
+        optionCard.setAttribute('data-idx', idx);
+        optionCard.onclick = () => handleAnswer(optionCard, option.answer, questionData.options);
+        optionCard.innerHTML = `
+          <div class="card-body">
+            <img src="${option.img}" class="img-fluid">
+            <p class="mt-2 font-weight-bold">${option.text}</p>
+          </div>`;
+        optionsDiv.appendChild(optionCard);
+      });
+      startTimer();
+    } else {
+      document.getElementById('question').innerText = '🎉 Quiz Finished! Your score is ' + score;
+      document.getElementById('options').innerHTML = '';
+      document.getElementById('result').innerText = '';
+      clearInterval(timerInterval);
+      document.getElementById('timerDisplay').style.display = 'none';
+      if ('speechSynthesis' in window) window.speechSynthesis.cancel();
+    }
+  }
+
+  function handleAnswer(selectedCard, isCorrect, options) {
+    const allCards = document.querySelectorAll('.card');
+    allCards.forEach(c => {
+      c.classList.add('disabled');
+      c.onclick = null;
+    });
+    clearInterval(timerInterval);
+
+    if (isCorrect) {
+      selectedCard.classList.add('bg-success');
+      score++;
+    } else {
+      selectedCard.classList.add('bg-danger');
+      allCards.forEach(card => {
+        if (card.getAttribute('data-answer') === 'true') {
+          card.classList.add('bg-success');
+        }
+      });
+    }
+
+    const correctOption = options.find(opt => opt.answer);
+
+    setTimeout(() => {
+      showPronounceModal(correctOption.text);
+    }, 500);
+  }
+
+  function startTimer() {
+    let timeLeft = 15;
+    const timerDisplay = document.getElementById('timerDisplay');
+    timerDisplay.classList.remove('urgency');
+    timerDisplay.innerText = timeLeft;
+    timerInterval = setInterval(() => {
+      timeLeft--;
+      timerDisplay.innerText = timeLeft;
+      if (timeLeft <= 5) {
+        timerDisplay.classList.add('urgency');
+      }
+      if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        const allCards = document.querySelectorAll('.card');
+        allCards.forEach(c => {
+          c.classList.add('disabled');
+          c.onclick = null;
+          if (c.getAttribute('data-answer') === 'true') {
+            c.classList.add('bg-success');
+          }
+        });
+        setTimeout(() => {
+          let correctOption = questions[currentQuestionIndex].options.find(opt => opt.answer);
+          showPronounceModal(correctOption.text);
+        }, 500);
+      }
+    }, 1000);
+  }
+
+  loadQuestion();
+  
